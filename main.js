5a648dc5827c2d48b40e4e2e512d64ae604f6643
Vadim A. Kalushko <116939084+kalushko26@users.noreply.github.com>
2023-02-28 15:06:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,101 @@
+const input = document.querySelector(".wrapper__search");
+const state = document.querySelector(".wrapper__status");
+const resultsContainer = document.querySelector(".wrapper__results");
+const reposContainer = document.querySelector(".wrapper__repos");
+const debRep = debounce(getRepos, 500);
+let controller;
+let signal;
+
+input.addEventListener("input", async () => {
+  removeTxt();
+  controller = new AbortController();
+  signal = controller.signal;
+  if (input.value.length == 0) controller.abort();
+  state.textContent = input.value.length == 0 ? `` : `Ожидание`;
+  await debRep(input.value)
+    .then((res) => {
+      if (input.value == "") return;
+      state.textContent = `${
+        res.length != 0
+          ? `Показано ${res.length} репозиториев`
+          : `Репозиторий не найден!`
+      }`;
+      removeTxt();
+      listener(res);
+    })
+    .catch((e) => {
+      if (e.name === "AbortError") {
+        alert(`я отменяю запрос на сервер`);
+      } else {
+        alert("Ошибка API");
+      }
+    });
+});
+
+async function getRepos(name) {
+  const repos = await fetch(
+    `https://api.github.com/search/repositories?q=${name}&per_page=5`,
+    { signal }
+  );
+  const response = await repos.json();
+  return response["items"];
+}
+
+/// Создание предложения для выбора
+function createTxt(text) {
+  const txtcont = document.createElement("div");
+  txtcont.textContent = text;
+  txtcont.classList.add("results__txt");
+  resultsContainer.appendChild(txtcont);
+  return txtcont;
+}
+
+function removeTxt() {
+  const cards = document.querySelectorAll(".results__txt");
+  for (let el of cards) {
+    el.remove();
+  }
+}
+
+function createRepo(name, owner, stars) {
+  const block = document.createElement("div");
+  const content = document.createElement("div");
+  const button = document.createElement("button");
+  content.textContent = `Name: ${name} 
+  Owner: ${owner} 
+  Stars: ${stars}`;
+  block.classList.add("result__content");
+  block.classList.add("result__block");
+  button.classList.add("repos__button");
+  content.classList.add("test");
+  block.append(content, button);
+  reposContainer.appendChild(block);
+  button.addEventListener("click", function () {
+    button.removeEventListener("click", arguments.callee);
+    block.remove();
+  });
+}
+
+function listener(res) {
+  for (let el of res) {
+    const txt = createTxt(el.name);
+    const { owner, name, stargazers_count: stars } = el;
+    txt.addEventListener("click", function () {
+      txt.removeEventListener("click", arguments.callee);
+      createRepo(name, owner.login, stars);
+      removeTxt();
+      input.value = "";
+      state.textContent = "";
+    });
+  }
+}
+/// Debouce
+function debounce(fn, ms) {
+  let timeout;
+  return (arg) => {
+    clearTimeout(timeout);
+    return new Promise((resolve) => {
+      timeout = setTimeout(() => resolve(fn(arg)), ms);
+    });
+  };
+}
